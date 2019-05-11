379890730429527c713e8ee2047c269844876ef1
sduit <kabirov.z@rahmetapp.kz>
2019-05-11 12:03:53 +0600

    changed Title of main page

--- Project/main.php
+++ Project/main.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 
-	<title>Main</title>
+	<title>Hello World</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="keywords" content="HTML, CSS, XML, XHTML, JavaScript">
